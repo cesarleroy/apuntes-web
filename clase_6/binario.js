cab29ca99ac537c89b32019f5aa26720397d6e3d
Cesar Carmona <clcg852@gmail.com>
2025-10-02 21:37:29 -0600

    corrigiendo archivos

--- clase_6/binario.js
+++ clase_6/binario.js
@@ -1,17 +1,17 @@
 function dec2bin(numero) {
-    let binario = "";
-    do {
-        if (numero % 2 == 0) {
-            numero /= 2;
-            binario = "0" + binario;
-        } else {
-            numero -= 1;
-            numero /= 2;
-            binario = "1" + binario;
-        }
-    } while (numero != 0);
+  let binario = "";
+  do {
+    if (numero % 2 == 0) {
+      numero /= 2;
+      binario = "0" + binario;
+    } else {
+      numero -= 1;
+      numero /= 2;
+      binario = "1" + binario;
+    }
+  } while (numero != 0);
 
-    return binario;
+  return binario;
 }
 
 console.log(dec2bin(22));
