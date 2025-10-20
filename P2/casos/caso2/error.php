55cc374c8d90d37dd397813a6d5521f55960b6b0
Cesar Carmona <clcg852@gmail.com>
2025-10-19 21:16:10 -0600

    19.10.25

---
+++ P2/casos/caso2/error.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <style>
+      body {
+        font-family: system-ui;
+      }
+    </style>
+  </head
+  <body>
+    <h1>Error</h1>
+  </body>
+</html>
---
+++ P2/casos/caso2/login.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!DOCTYPE>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <style>
+      body {
+        font-family: system-ui;
+      }
+    </style>
+  </head
+  <body>
+    <form method="POST" action="home.php">
+      <label>User:</label>
+      <input type="email" name="user"/><br/>
+      <label>Password:</label>
+      <input type="password" name="pass"/><br/>
+      <button>Enviar</button>
+    </form>
+  </body>
+</html>
